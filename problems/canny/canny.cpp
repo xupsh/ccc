a036ffccd52d9d1ae9fbdf5a7fb1719238e7756b
Maxwellhyh <xup.shanghai@gmail.com>
2022-04-14 23:36:10 +0800

    Update problems

--- problems/canny/canny.cpp
+++ problems/canny/canny.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 void canny(int* src, int* dst, int upperThresh, int lowerThresh)
 {
     //Write your code here
---
+++ problems/serialization/top.cpp
@@ -0,0 +1,9 @@
+#include "top.hpp"
+#include <ap_int.h>
+
+void dut(char* src_buff, int src_sz, char* dst_buff) {
+
+// Please write your code here
+
+
+}
