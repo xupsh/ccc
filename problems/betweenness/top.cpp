bd0d88903f73fd07372d68bb4e1b930962207f0e
Tianmin Rao <tianminr@xilinx.com>
2022-05-23 05:41:22 -0700

    modify top.cpp for tb

--- problems/betweenness/top.cpp
+++ problems/betweenness/top.cpp
@@ -1,18 +1,12 @@
 #include "top.hpp"
+#include <list>
 #include <queue>
 #include <stack>
-#include <list>
 
-void dut(unsigned numVert,
-         unsigned numEdge,
-         unsigned* offset,
-         unsigned* column,
-         float* btwn,
-         unsigned* tmp0,
-         unsigned* tmp1,
-         unsigned* tmp2,
-         unsigned* tmp3) {
-    // clang-format off
+void dut(unsigned numVert, unsigned numEdge, unsigned *offset, unsigned *column,
+         float *btwn, unsigned *tmp0, unsigned *tmp1, unsigned *tmp2,
+         unsigned *tmp3) {
+  // clang-format off
 
     const unsigned MEMSIZE=INTERFACE_MEMSIZE;
 #pragma HLS INTERFACE m_axi offset = slave latency = 32 num_write_outstanding = 1 num_read_outstanding = \
@@ -38,61 +32,65 @@ void dut(unsigned numVert,
 
 // clang-format on
 #ifndef __SYNTHESIS__
-    for (int i = 0; i < numVert; i++) {
-        btwn[i] = 0;
+  for (int i = 0; i < numVert; i++) {
+    btwn[i] = 0;
+  }
+  for (int i = 0; i < numVert; i++) {
+    std::stack<unsigned> s;
+    std::vector<std::list<unsigned>> p(numVert);
+    std::vector<float> sigma(numVert);
+    std::vector<int> dist(numVert);
+    std::queue<unsigned> q;
+    unsigned source = i;
+
+    for (int j = 0; j < numVert; j++) {
+      sigma[j] = 0;
+      dist[j] = -1;
     }
-    for (int i = 0; i < numVert; i++) {
-        std::stack<unsigned> s;
-        std::vector<std::list<unsigned> > p(numVert);
-        std::vector<float> sigma(numVert);
-        std::vector<int> dist(numVert);
-        std::queue<unsigned> q;
-        unsigned source = i;
+    sigma[source] = 1;
+    dist[source] = 0;
 
-        for (int j = 0; j < numVert; j++) {
-            sigma[j] = 0;
-            dist[j] = -1;
+    q.push(source);
+    while (!q.empty()) {
+      unsigned v = q.front();
+      s.push(v);
+      for (int j = offset[v]; j < offset[v + 1]; j++) {
+        unsigned w = column[j];
+        if (dist[w] < 0) {
+          q.push(w);
+          dist[w] = dist[v] + 1;
         }
-        sigma[source] = 1;
-        dist[source] = 0;
-
-        q.push(source);
-        while (!q.empty()) {
-            unsigned v = q.front();
-            s.push(v);
-            for (int j = offset[v]; j < offset[v + 1]; j++) {
-                unsigned w = column[j];
-                if (dist[w] < 0) {
-                    q.push(w);
-                    dist[w] = dist[v] + 1;
-                }
-                if (dist[w] == dist[v] + 1) {
-                    sigma[w] = sigma[w] + sigma[v];
-                    p[w].push_back(v);
-                }
-            }
-            q.pop();
+        if (dist[w] == dist[v] + 1) {
+          sigma[w] = sigma[w] + sigma[v];
+          p[w].push_back(v);
         }
-
-        std::vector<float> delta(numVert);
-        for (int j = 0; j < numVert; j++) {
-            delta[j] = 0;
-        }
-        while (!s.empty()) {
-            unsigned w = s.top();
-            for (std::list<unsigned>::iterator it = p[w].begin(); it != p[w].end(); it++) {
-                unsigned v = *it;
-                delta[v] = delta[v] + (sigma[v] / sigma[w]) * (1 + delta[w]);
-                if (source != w) {
-                    btwn[w] = btwn[w] + delta[w];
-                }
-            }
-            s.pop();
-        }
+      }
+      q.pop();
     }
+
+    std::vector<float> delta(numVert);
+    for (int j = 0; j < numVert; j++) {
+      delta[j] = 0;
+    }
+    while (!s.empty()) {
+      unsigned w = s.top();
+      if (source != w) {
+        btwn[w] = btwn[w] + delta[w];
+      }
+      for (std::list<unsigned>::iterator it = p[w].begin(); it != p[w].end();
+           it++) {
+        unsigned v = *it;
+        delta[v] = delta[v] + (sigma[v] / sigma[w]) * (1 + delta[w]);
+        // if (source != w) {
+        //     btwn[w] = btwn[w] + delta[w];
+        // }
+      }
+      s.pop();
+    }
+  }
 #else
-    for (int i = 0; i < numVert; i++) {
-        btwn[i] = i;
-    }
+  for (int i = 0; i < numVert; i++) {
+    btwn[i] = i;
+  }
 #endif
 }
